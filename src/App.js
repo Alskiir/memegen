7675f4dba76a513ef0b16596558695cf6a8961f4
Alskiir <doughart215@cfl.rr.com>
2023-07-13 15:55:54 -0400

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import './App.css';
-import memesData from './components/meme/memesData';
 
 import { Header, Meme } from './components/App';
 
 function App() {
-  // const memeElements = memesData.map(item => {
-  //   return (
-  //     <Meme 
-  //     {...item}
-  //     {...item.data.memes}
-  //     />
-  //   )
-  // })
-
-  
-  
   return (
     <div className='container'> 
       <Header />
       <Meme/>
-      {/* <section>
-        {memeElements}
-      </section> */}
     </div>
   );
 }
--- src/components/meme/Meme.js
+++ src/components/meme/Meme.js
@@ -1,8 +1,7 @@
 import React from "react";
 import './meme.css';
-import memesData from "./memesData";
 
-function Meme(props) {
+function Meme() {
 
     const [meme, setMeme] = React.useState({
         topText: "",
@@ -10,16 +9,31 @@ function Meme(props) {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme (prevState => ({
             ...prevState,
             randomImage: url
         }))
+    }
+
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
     }
 
     return (
@@ -28,11 +42,17 @@ function Meme(props) {
             <input className="form--input"
                 type="text"
                 placeholder="Top text"
+                name="topText"
+                onChange={handleChange}
+                value={meme.topText}
             />
 
             <input className="form--input"
                 type="text"
                 placeholder="Bottom text"
+                name="bottomText"
+                onChange={handleChange}
+                value={meme.bottomText}
             />
 
             <button className="form--button"
@@ -41,7 +61,11 @@ function Meme(props) {
             </button>
         </div>
         
-        <img className="meme--image" src={meme.randomImage} />
+        <div className="meme">
+            <img className="meme--image" src={meme.randomImage} />
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
         </main>
     )
 }
